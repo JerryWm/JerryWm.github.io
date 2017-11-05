0e9d8909f83c072a99003176b805e247eb9d9af6
JerryWm <33358335+JerryWm@users.noreply.github.com>
2017-11-05 12:35:54 +0500

    Add files via upload

--- xmr/app/versions.js
+++ xmr/app/versions.js
@@ -1,5 +1,6 @@
 window.appVersions = [
-	{version: "1.2.0", latest: true, download: "build/v1.2.0.zip"},
+	{version: "1.2.1", latest: true, description: "v1.2.1/description/index.html", download: "build/v1.2.1.zip"},
+	{version: "1.2.0", download: "build/v1.2.0.zip"},
 	{version: "1.1.0"},
 	{version: "1.0.1"},
 	//{version: "1.0.0"},
